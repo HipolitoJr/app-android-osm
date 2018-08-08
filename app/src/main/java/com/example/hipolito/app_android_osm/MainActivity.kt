063389e163ac3e03870fa049b6da4bb7e7a59496
HipolitoJr <hipolitocavalcantejr@gmail.com>
2018-08-07 21:55:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/hipolito/app_android_osm/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hipolito.app_android_osm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
