7ea6e4a73b7875882fc758b8143e06ac4ce04fe8
HipolitoJr <hipolitocavalcantejr@gmail.com>
2018-08-08 00:50:15 -0300

    Implementação do home

--- app/src/main/java/com/example/hipolito/app_android_osm/app/MainActivity.kt
+++ app/src/main/java/com/example/hipolito/app_android_osm/app/MainActivity.kt
@@ -2,12 +2,66 @@ package com.example.hipolito.app_android_osm.app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.Toast
 import com.example.hipolito.app_android_osm.R
+import com.example.hipolito.app_android_osm.infra.api.APIService
+import com.example.hipolito.app_android_osm.model.Refeicao
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var apiService: APIService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initComponents()
     }
+
+    private fun initComponents() {
+        apiService = APIService("")
+        capturarDiaSelecionado()
+    }
+
+    private fun buscarCardapio(data: String) {
+        val call = apiService.cardapioEndPoint.getRefeicoes(data)
+
+        call.enqueue(object: Callback<Refeicao>{
+            override fun onResponse(call: Call<Refeicao>?, response: Response<Refeicao>?) {
+                if (response!!.isSuccessful){
+                    exibirCardapio(response.body()!!)
+                }else{
+                    Toast.makeText(this@MainActivity, "Erro " + response!!.code(), Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<Refeicao>?, t: Throwable?) {
+                Toast.makeText(this@MainActivity, "Falha na conexão", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun exibirCardapio(refeicao: Refeicao) {
+        Toast.makeText(this@MainActivity, refeicao.tipo, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun capturarDiaSelecionado() {
+        calendarCardapios.setOnDateChangeListener { calendarView, year, month, day ->
+            var data = Date(year, month, day)
+
+            buscarCardapio(converterData(Date(calendarView.date)))
+        }
+    }
+
+    private fun converterData(date: Date): String {
+        val formatBra = SimpleDateFormat("dd/MM/2018")
+        return formatBra.format(date)
+    }
+
 }
--- app/src/main/java/com/example/hipolito/app_android_osm/infra/api/APIService.kt
+++ app/src/main/java/com/example/hipolito/app_android_osm/infra/api/APIService.kt
@@ -1,15 +1,18 @@
 package com.example.hipolito.app_android_osm.infra.api
 
+import com.example.hipolito.app_android_osm.infra.api.endpoints.CardapioEndPoint
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class APIService {
 
-    private val BASE_URL = "http://192.168.43.128:8000/api/v1/"
+    private val BASE_URL = "http://192.168.100.46:8000/api/v1/"
 
     private lateinit var retrofit: Retrofit
     private lateinit var interceptorAPI: InterceptorAPI
+
+    public lateinit var cardapioEndPoint: CardapioEndPoint
 
     constructor(Token: String) {
 
@@ -25,5 +28,7 @@ class APIService {
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(usuario)
                 .build()
+
+        cardapioEndPoint = this.retrofit.create(CardapioEndPoint::class.java)
     }
 }
